f17939990c6401d0fb969cb5aced0ccccf32c5ca
Konrad Kühne <konnykuehne@gmail.com>
2017-02-27 16:42:06 +0100

    initialize project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject antenne "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/clojurescript "1.9.494"]
+                 [cljs-ajax "0.5.8"]])
